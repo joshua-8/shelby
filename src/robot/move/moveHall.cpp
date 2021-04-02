f844d0c12c115dc6387d1e5455ad87638b3c1df3
joshua-8 <59814881+joshua-8@users.noreply.github.com>
2021-04-01 18:24:38 -0700

    started creating moveHall class

---
+++ src/robot/move/moveHall.cpp
@@ -0,0 +1,9 @@
+#include "moveHall.h"
+#include "robot/robot.h"
+MoveDrive::MoveDrive()
+{
+
+}
+void MoveDrive::run()
+{
+}
---
+++ src/robot/move/moveHall.h
@@ -0,0 +1,12 @@
+#ifndef MOVEHALL_H
+#define MOVEHALL_H
+#include "shelbytron_globs.h"
+#include <Arduino.h>
+class MoveHall {
+public:
+    MoveHall();
+
+private:
+
+};
+#endif
